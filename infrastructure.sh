6e7a70cc98521f49c2495749f665c73e238b8271
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-12 00:11:19 +0100

    HOCS-5331: add elasticsearch status log

--- infrastructure.sh
+++ infrastructure.sh
@@ -7,8 +7,10 @@ then
 else
   if [[ $2 == "true" ]]
   then
+      echo "Starting with elastic search"
       docker-compose -f ./ci/docker-compose.yml -f ./ci/docker-compose-elastic.yml up -d "$1"
   else
+      echo "Starting without elastic search"
       docker-compose -f ./ci/docker-compose.yml up -d "$1"
   fi
   until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
