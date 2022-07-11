a00e4d59b98e198e978866491111866113795a9c
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-12 00:03:09 +0100

    HOCS-5331: missing 'up'

--- infrastructure.sh
+++ infrastructure.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo
 
-if [[ $1 != "" ]]
+if [[ $1 == "" ]]
 then
+  echo "No docker-compose services listed, not setting up infrastructure!"
+else
   if [[ $2 == "true" ]]
   then
-      docker-compose -f docker-compose.yml -f docker-compose-elastic.yml -d "$1"
+      docker-compose up -f docker-compose.yml -f docker-compose-elastic.yml -d "$1"
   else
-      docker-compose -f docker-compose.yml -d "$1"
+      docker-compose up -f docker-compose.yml -d "$1"
   fi
   until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
        sleep 5
        echo "Waiting for LocalStack Initialisation scripts to have been run..."
   done
-else
-  echo "No integration tests, not setting up infrastructure!"
 fi
