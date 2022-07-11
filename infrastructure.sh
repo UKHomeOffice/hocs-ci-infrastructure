29eb1b862f4f0a9f61a5a0de275a2064c8d8d94d
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-12 00:05:34 +0100

    HOCS-5331: missing 'up'

--- infrastructure.sh
+++ infrastructure.sh
@@ -7,9 +7,9 @@ then
 else
   if [[ $2 == "true" ]]
   then
-      docker-compose up -f docker-compose.yml -f docker-compose-elastic.yml -d "$1"
+      docker-compose -f docker-compose.yml -f docker-compose-elastic.yml up -d "$1"
   else
-      docker-compose up -f docker-compose.yml -d "$1"
+      docker-compose -f docker-compose.yml up -d "$1"
   fi
   until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
        sleep 5
