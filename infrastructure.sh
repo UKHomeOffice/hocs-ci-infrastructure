2d7db4b4ee744a61869681452bc11c999e1b72d2
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-11 23:58:13 +0100

    HOCS-5331: include wait into infrastructure.sh

--- infrastructure.sh
+++ infrastructure.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -euo
 
-if[[ -z $2 ]]
+if [[ $1 != "" ]]
 then
-  docker-compose -f docker-compose.yml -d $1
+  if [[ $2 == "true" ]]
+  then
+      docker-compose -f docker-compose.yml -f docker-compose-elastic.yml -d "$1"
+  else
+      docker-compose -f docker-compose.yml -d "$1"
+  fi
+  until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
+       sleep 5
+       echo "Waiting for LocalStack Initialisation scripts to have been run..."
+  done
 else
-  docker-compose -f docker-compose.yml -f docker-compose-elastic.yml -d $1
+  echo "No integration tests, not setting up infrastructure!"
 fi
