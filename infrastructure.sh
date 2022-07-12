dd3addb63de2c4c920708dad2818eb9fb27e6d8e
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-07-12 15:44:35 +0100

    HOCS-5331: fix infrastructure.sh param error

--- infrastructure.sh
+++ infrastructure.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -euo
 
-if [[ $1 == "" ]]
+if [[ ${COMPOSE_SERVICES} == "" ]]
 then
   echo "No docker-compose services listed, not setting up infrastructure!"
 else
-  if [[ $2 == "true" ]]
+  if [[ ${INCLUDE_ELASTIC} == "true" ]]
   then
       echo "Starting with elastic search"
-      docker-compose -f ./ci/docker-compose.yml -f ./ci/docker-compose-elastic.yml up -d "$1"
+      docker-compose -f ./ci/docker-compose.yml -f ./ci/docker-compose-elastic.yml up -d ${COMPOSE_SERVICES}
   else
       echo "Starting without elastic search"
-      docker-compose -f ./ci/docker-compose.yml up -d "$1"
+      docker-compose -f ./ci/docker-compose.yml up -d ${COMPOSE_SERVICES}
   fi
+
   until curl 'localhost.localstack.cloud:4566/health' --silent | grep -q "\"initScripts\": \"initialized\""; do
        sleep 5
        echo "Waiting for LocalStack Initialisation scripts to have been run..."
