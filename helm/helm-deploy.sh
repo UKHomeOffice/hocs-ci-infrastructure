ef4b8034675b3accb32f9ab8b3c52c7f764549a8
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2022-09-16 23:56:53 +0100

    ✨ HOCS-5561: Add shared CI helm script (#32)
    * Update helm-deploy.sh
    
    * Delete setup-kube.sh

--- helm/helm-deploy.sh
+++ helm/helm-deploy.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+wget -O /cluster_ca.crt \
+ https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/${KUBE_CLUSTER}.crt
+
+kubectl config set-cluster "${KUBE_CLUSTER}" \
+  --certificate-authority="/cluster_ca.crt" \
+  --server="${KUBE_SERVER}"
+
+kubectl config set-credentials helm \
+  --token="${KUBE_TOKEN}"
+
+kubectl config set-context helm \
+  --cluster="${KUBE_CLUSTER}" \
+  --user="helm" \
+
+kubectl config use-context helm
+
 helm repo add hocs-helm-charts https://ukhomeoffice.github.io/hocs-helm-charts
 
 helm dependency update ./helm/${CHART_NAME}
