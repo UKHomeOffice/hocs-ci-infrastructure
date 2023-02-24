01f5c33aad7fd8adc012063fbda925c515554848
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2023-02-24 14:17:59 +0000

    Update localstack version in docker-compose
    This change updates the localstack version within the docker-compose
    files to the latest 1.4.0 version.
    
    There are significant changes included in this version upgrade:
    - Changes to healthcheck endpoint as previous is deprecated
    - Removal of `DEFAULT_REGION` as deprecated
    - Change to init folder as previous is deprecated.
    
    Other changes include:
    - Marking scripts with executable permissions
    - Change to service check to regex against either `available` and
      `running`.
    
    This change also includes moving to OpenSearch from ElasticSearch. The
    domain is created with version 1.3, which we are running in
    environments. To support this, a better domain ready check has been
    added.

--- localstack/3-setup-elastic.sh
+++ localstack/3-setup-elastic.sh
@@ -6,9 +6,9 @@ export AWS_SECRET_ACCESS_KEY=UNSET
 export AWS_DEFAULT_REGION=eu-west-2
 
 ## make sure that localstack is running in the pipeline
-until curl http://localstack:4566/health --silent | grep -q "\"es\": \"available\""; do
+until curl http://localstack:4566/_localstack/health --silent | grep -q "\"opensearch\": \"available\""; do
    sleep 5
    echo "Waiting for LocalStack to be ready..."
 done
 
-awslocal es create-elasticsearch-domain --domain-name decs
+awslocal opensearch create-domain --engine-version "OpenSearch_1.3" --domain-name decs
